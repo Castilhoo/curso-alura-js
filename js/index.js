c137d4ab94c1d1fedc2794784dcc9e5966b709dc
Rafael Castilho <castilho-rafael@outlook.com>
2018-08-21 15:54:37 -0300

    começando a calcular imc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 var titulo = document.querySelector('.titulo');
-
 titulo.textContent = 'Aparecida Nutricionista';
+
+var patientOne = document.querySelector('#patient-one');
+
+var tdPeso = patientOne.querySelector('.info-peso')
+var peso = tdPeso.textContent;
+
+var tdAltura = patientOne.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var imc = peso/(altura*altura);
+
+var tdImc = patientOne.querySelector('.info-imc');
+tdImc.textContent = imc;
+
+console.log(imc);
