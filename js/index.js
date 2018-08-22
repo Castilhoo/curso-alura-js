9777ddf82bf70d03e724f6e800591180afd1372b
Rafael Castilho <castilho-rafael@outlook.com>
2018-08-22 18:41:38 -0300

    add new patient (appendChild, createElement)

--- js/index.js
+++ js/index.js
@@ -18,14 +18,12 @@ for(var i = 0; i < patients.length ; i++) {
     var alturaValida = true;
 
     if(peso <= 0 || peso >= 500) {
-        console.log('Peso Inválido!');
         pesoValido = false;
         tdImc.textContent = 'Peso inválido!';
         patient.classList.add('paciente-invalido');
     }
 
     if(altura <= 0 || altura >= 3.00) {
-        console.log('Altura Inválida!');
         alturaValida = false;
         tdImc.textContent = 'Altura inválida!';
         patient.classList.add('paciente-invalido');
@@ -36,6 +34,42 @@ for(var i = 0; i < patients.length ; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var adicionar = document.querySelector('#adicionar-paciente');
+
+adicionar.addEventListener('click', function(event){
+    event.preventDefault();
+
+    var form = document.querySelector('#form-adiciona');
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement('tr');
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector('#tabela-pacientes');
+    tabela.appendChild(pacienteTr);
+
+
+});
 
 
 
