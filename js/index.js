1dd25cdba681df270587bade5f0c390989f4613f
Rafael Castilho <castilho-rafael@outlook.com>
2018-08-23 15:56:14 -0300

    separando js

--- js/index.js
+++ js/index.js
@@ -35,41 +35,7 @@ for(var i = 0; i < patients.length ; i++) {
     }
 }
 
-var adicionar = document.querySelector('#adicionar-paciente');
 
-adicionar.addEventListener('click', function(event){
-    event.preventDefault();
-
-    var form = document.querySelector('#form-adiciona');
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement('tr');
-    var nomeTd = document.createElement('td');
-    var pesoTd = document.createElement('td');
-    var alturaTd = document.createElement('td');
-    var gorduraTd = document.createElement('td');
-    var imcTd = document.createElement('td');
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = imc;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
-    var tabela = document.querySelector('#tabela-pacientes');
-    tabela.appendChild(pacienteTr);
-
-
-});
 
 
 
