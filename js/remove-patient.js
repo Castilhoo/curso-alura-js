7522923f84c78024cbf90ac47404840c2a13bc8b
Rafael Castilho <castilho-rafael@outlook.com>
2018-09-17 16:09:41 -0300

    removendo paciente da tabela

--- js/remove-patient.js
+++ js/remove-patient.js
@@ -1,1 +1,13 @@
 var pacientes = document.querySelectorAll('.paciente');
+
+var tabela = document.querySelector('table');
+
+tabela.addEventListener('dblclick', function(event){
+    event.target.parentNode.remove();
+});
+
+// pacientes.forEach(function(paciente){
+//   paciente.addEventListener('dblclick', function(){
+//        this.remove();
+//    })
+// });
